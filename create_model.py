007d54e94bdfe0ef9e430385fafbdb77c63a73d7
Ketil Malde <ketil@malde.org>
2019-02-28 11:48:31 +0100

    Automatically decide size of predictions layer from data

--- create_model.py
+++ create_model.py
@@ -3,15 +3,24 @@ from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.optimizers import SGD
 
+import os
+
+import config as C
+
 def create_model():
     # Load Inception minus the final prediction layer
     base_model = InceptionV3(weights='imagenet',
                          include_top=False,
                          input_shape=(299, 299, 3))  # or 1?
+    tcls = os.listdir(C.train_dir)
+    vcls = os.listdir(C.val_dir)
+    if len(tcls) != len(vcls):
+        print('Error: mismatch between training and validation classes')
+        exit -1
 
-    # Add a new 84-class prediction layer
+    # Add a new prediction layer
     tmp = GlobalAveragePooling2D()(base_model.output)
-    predictions = Dense(60, activation='softmax')(tmp)
+    predictions = Dense(len(tcls), activation='softmax')(tmp)
     model = Model(inputs=base_model.input, outputs=predictions)
 
     return model
