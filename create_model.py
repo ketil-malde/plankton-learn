5e7a99c420f0e55fe90932c660a8c20e7e12ae79
Ketil Malde <ketil@malde.org>
2019-02-28 11:01:45 +0100

    Cleanup/refactor to better match plankton-siamese layout

--- create_model.py
+++ create_model.py
@@ -3,20 +3,15 @@ from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.optimizers import SGD
 
-# Load Inception minus the final prediction layer
-base_model = InceptionV3(weights='imagenet',
+def create_model():
+    # Load Inception minus the final prediction layer
+    base_model = InceptionV3(weights='imagenet',
                          include_top=False,
-                         input_shape=(299, 299, 3))
-# Use 299x299x1 since monochromatic?
+                         input_shape=(299, 299, 3))  # or 1?
 
-# Add a new 84-class prediction layer
-tmp = GlobalAveragePooling2D()(base_model.output)
-predictions = Dense(60, activation='softmax')(tmp)
-model = Model(inputs=base_model.input, outputs=predictions)
+    # Add a new 84-class prediction layer
+    tmp = GlobalAveragePooling2D()(base_model.output)
+    predictions = Dense(60, activation='softmax')(tmp)
+    model = Model(inputs=base_model.input, outputs=predictions)
 
-# we use SGD with a low learning rate
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
-                      loss='categorical_crossentropy',
-                                    metrics=['mse', 'accuracy'])
-
-model.save('models/initial.model')
+    return model
